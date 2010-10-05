b94be425c0c0244e0bdee8e1b20e9020c22818cc
nakaji <nakaji4923@gmail.com>
2010-10-05 15:02:54 +0900

    add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
-  validates_presence_of :name
+  validates_presence_of :name, :description, :image_url
+  validates_numericality_of :price, :greater_than => 0, :only_integer => true
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|jpeg|png)\Z}i,
+                      :message => " は GIF,JPG,PNG形式のurlでなくてはいけません" 
 end
