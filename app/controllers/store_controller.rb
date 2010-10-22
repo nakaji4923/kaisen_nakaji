a7db7598d324f8afec5b76d2ebefcece47d42707
nakaji <nakaji4923@gmail.com>
2010-10-22 11:50:48 +0900

    add cart function.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,4 +3,21 @@ class StoreController < ApplicationController
     @products = Product.for_sale.paginate :page => params[:page], :per_page => 3
   end
 
+  def add_to_cart
+    @product = Product.find(params[:id])
+    @cart = current_cart
+    @cart.add_product(@product)
+  end
+
+  private
+  def current_cart
+    session[:cart] ||= Cart.new
+
+#    if session[:cart].nil?
+#      Cart.new
+#    else
+#      session[:cart]
+#    end
+
+  end
 end
