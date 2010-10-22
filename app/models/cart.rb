748935a8db53f73c91f98e4237694b90e79b1466
nakaji <nakaji4923@gmail.com>
2010-10-22 14:04:26 +0900

    BUG fixed.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,22 @@
 class Cart
-  attr_reader :items
+  # attr_reader :items
 
-  def initialize
-    @items = []
-  end
+#  def initialize
+#    @item_ids = []
+#  end
 
   def add_product(product)
-    @items << product
+    item_ids << product.id
+  end
+
+  def items
+#    Product.find(@item_ids)
+    item_ids.map{|id| Product.find(id)}
+  end
+
+  private
+  def item_ids
+    @item_ids ||= []
   end
 end
 
