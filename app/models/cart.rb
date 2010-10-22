3f0d5b44271d49f3d0a3c4a8c54faf13340b3797
nakaji <nakaji4923@gmail.com>
2010-10-22 15:00:08 +0900

    append items times.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,7 +6,8 @@ class Cart
 #  end
 
   def add_product(product)
-    item_ids << product.id
+#    item_ids << product.id
+   quantities_by_item_id[product.id] += 1
   end
 
   def items
@@ -14,9 +15,20 @@ class Cart
     item_ids.map{|id| Product.find(id)}
   end
 
+  def items_with_quantity
+    quantities_by_item_id.map do |product_id, quantity|
+      [Product.find(product_id), quantity]
+    end
+  end
+
+
   private
-  def item_ids
-    @item_ids ||= []
+#  def item_ids
+#    @item_ids ||= []
+#  end
+
+  def quantities_by_item_id
+    @quantities_by_item_id ||= Hash.new(0)
   end
 end
 
